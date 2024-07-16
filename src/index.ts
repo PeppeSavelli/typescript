60f24ce01dda0cbf0b9e70dbccec7d6d1b2fbb85
PeppeSavelli <peppe070904@gmail.com>
2024-07-16 15:10:13 +0200

    Doc:Added files

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,49 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
+import { createClient } from "@vercel/postgres";
 
-    const app = express();
-    const port = process.env.PORT || 3000;
-    const server = express.json();
+const client = createClient(
+{connectionString: "postgres://default:I13qNChxetcJ@ep-young-dawn-a2mpjtzn.eu-central-1.aws.neon.tech:5432/verceldb"}
+);
 
-    app.use(server);
+const app = express();
+const server = express.json();
+const port = process.env.PORT || 3000;
+const baseURL = process.env.BASE_URL || "http://localhost";
 
-    
+app.use(server);
 
-    app.listen(port, () => {
+app.get("/posts", function (req: Request, res: Response) {
+  return res.status(200).json([]);
+});
+
+app.get("/posts/:idPost", async function (request: Request, response: Response) {
+  await client.connect();
+  const res = await client.query("SELECT * FROM posts");
+  return response.status(200).json(res.rows);
+});
+
+app.delete("/posts/:idPost", function (req: Request, res: Response) {
+    return res.status(200).json({
+      message: `Post ${req.params.idPost} deleted`
+    });
+  });
+  
+  app.put("/posts/:idPost", function (req: Request, res: Response) {
+    return res.status(200).json({
+      id : req.body.id,
+      title: req.body.title,
+      content: req.body.content
+    });
+  });
+  
+  app.post("/posts", function (req: Request, res: Response) {
+    return res.status(201).json({
+      id: Math.random(),
+      title: req.body.title,
+      content: req.body.content
+    });
+  });
+
+app.listen(port, () => {
         console.log(`server is running on http://localhost:${port}`);
     })
